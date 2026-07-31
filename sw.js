5221e0acbcfc80984b850dda760fc2c3a2fe80e9
ncarbonia <nick.carbonia@biggsteelllc.com>
2026-07-31 15:23:20 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "tr13-field-app-v1";
+const CACHE_NAME = "tr13-field-app-v3";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css",
-  "./app.js",
+  "./styles.css?v=3",
+  "./app.js?v=3",
   "./manifest.webmanifest",
   "./icon.svg"
 ];
